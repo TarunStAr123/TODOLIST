286be7b0ca2df8ab2f2a09aeefe73006e890c8e1
TarunStAr123 <tarunmamilla2005@gmail.com>
2026-02-28 14:21:45 +0530

    fix(router): add basename configured by vite base to avoid github pages blank screen

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <ThemeProvider>
       <AuthProvider>
-        <BrowserRouter>
+        <BrowserRouter basename={import.meta.env.BASE_URL}>
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/login" element={<LoginPage />} />
